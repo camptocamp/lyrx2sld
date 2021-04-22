5297e2ad0f4da56ef02e0e1b5f81928f9ec6f49b
Cécile Vuilleumier <cecile.vuilleumier@camptocamp.com>
2021-04-22 12:02:51 +0200

    Fix bash syntax

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,10 @@
-#! /bin/bash
+#! /bin/bash -e
 folders=$(ls data/)
 input_file='input.lyrx'
 output_dir='/usr/share/geoserver/data_dir/styles/ag/'
 for folder in $folders
 do
-    echo 'Converting '$folder
-    output_file=$output_dir$folder'.sld'
-    curl -d @data/$folder/$input_file http://localhost/v1/lyrx2sld/?replaceesri=true -o $output_file
+    echo "Converting ${folder}"
+    output_file="${output_dir}${folder}.sld"
+    curl -d @"data/${folder}/${input_file}" "http://localhost/v1/lyrx2sld/?replaceesri=true" -o "${output_file}"
 done
