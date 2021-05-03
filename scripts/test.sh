30691adeda4bbab1e258a86097857f8a41e035e7
Cécile Vuilleumier <cecile.vuilleumier@camptocamp.com>
2021-05-03 10:07:49 +0200

    Create Docker composition which includes GeoServer for testing

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,10 @@
 #! /bin/bash -eu
-folders=$(ls data/)
+folders=$(ls data/ag/)
 input_file='input.lyrx'
-output_dir='/usr/share/geoserver/data_dir/styles/ag/'
+output_dir='data/geoserver_data_dir/styles/ag/'
 for folder in $folders
 do
     echo "Converting ${folder}"
     output_file="${output_dir}${folder}.sld"
-    curl -d @"data/${folder}/${input_file}" "http://localhost/v1/lyrx2sld/?replaceesri=true" -o "${output_file}"
+    curl -d @"data/ag/${folder}/${input_file}" "http://localhost/v1/lyrx2sld/?replaceesri=true" -o "${output_file}"
 done
