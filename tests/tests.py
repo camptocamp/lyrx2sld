b9a3de7bc690c906d78fef03a98a0a64f08a3982
volaya <volayaf@gmail.com>
2021-06-28 10:14:11 +0200

    added test for icon conversion

--- tests/tests.py
+++ tests/tests.py
@@ -4,10 +4,13 @@ from unittest import TestCase
 
 import json
 import io
+import os
 import zipfile
 import requests
 from xml.etree import ElementTree
 import ast
+
+APP_ENDPOINT = "http://localhost/v1/lyrx2sld"
 
 
 class SLDParser():
@@ -48,13 +51,18 @@ def get_style_from_zip_response(content):
             data = SLDParser(sld).as_dict()
             return data
 
+def input_test_file(filename):
+    return os.path.join(os.path.dirname(__file__), "data", filename)
+
+def expected_test_file(filename):
+    return os.path.join(os.path.dirname(__file__), "expected", filename)
 
 class TestService(TestCase):
 
     def test_point_symbology(self):
-        with open("./tests/data/input.lyrx") as f:
+        with open(input_test_file("input.lyrx")) as f:
             obj = json.load(f)
-        response = requests.post("http://localhost/v1/lyrx2sld", json=obj, timeout=30)
+        response = requests.post(APP_ENDPOINT, json=obj, timeout=30)
         self.assertEqual(response.status_code, 200)
         data = get_style_from_zip_response(response.content)
         point_symbolizer = data['NamedLayer']['UserStyle']['FeatureTypeStyle']['Rule']['PointSymbolizer']
@@ -62,11 +70,25 @@ class TestService(TestCase):
         self.assertEqual(point_symbolizer['Graphic']['Mark']['WellKnownName'], 'ttf://ESRI Default Marker#0x21')
 
     def test_point_symbology_replace_esri(self):
-        with open("./tests/data/input.lyrx") as f:
+        with open(input_test_file("input.lyrx")) as f:
             obj = json.load(f)
-        response = requests.post("http://localhost/v1/lyrx2sld?replaceesri=true", json=obj, timeout=30)
+        response = requests.post(f"{APP_ENDPOINT}?replaceesri=true", json=obj, timeout=30)
         self.assertEqual(response.status_code, 200)
         data = get_style_from_zip_response(response.content)
         point_symbolizer = data['NamedLayer']['UserStyle']['FeatureTypeStyle']['Rule']['PointSymbolizer']
         self.assertEqual(data['NamedLayer']['Name'], 'Bauinventarobjekte')
         self.assertEqual(point_symbolizer['Graphic']['Mark']['WellKnownName'], 'circle')
+
+    def test_icon_conversion(self):
+        with open(input_test_file("withicons.lyrx")) as f:
+            obj = json.load(f)
+        response = requests.post(APP_ENDPOINT, json=obj, timeout=30)
+        self.assertEqual(response.status_code, 200)
+        with zipfile.ZipFile(io.BytesIO(response.content)) as z:
+            zippedfiles = {zipinfo.filename for zipinfo in z.infolist()}
+            self.assertEqual({"style.sld", "0.png"}, zippedfiles)
+            with open(expected_test_file("icon.png"), "rb") as f:
+                expected = f.read()
+            with z.open('0.png') as f:
+                output = f.read()
+            self.assertEqual(expected, output)
