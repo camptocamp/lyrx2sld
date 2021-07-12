c7d71be10382fe17a001eb1fd0e3e39c7ed9c9a7
Cécile Vuilleumier <cecile.vuilleumier@camptocamp.com>
2021-07-12 14:10:07 +0200

    Add integration test to CI (#14)

---
+++ tests/integration_test.sh
@@ -0,0 +1,15 @@
+#! /bin/bash -eu
+
+SERVICE="http://localhost/v1/lyrx2sld/"
+INPUT_FILE="tests/data/withicons.lyrx"
+OUTPUT_FILE="/tmp/output.zip"
+
+STATUS_CODE=$(curl --write-out %{http_code} -v -d @$INPUT_FILE -o $OUTPUT_FILE $SERVICE)
+
+if [ -f "$OUTPUT_FILE" ] && [ $STATUS_CODE = 200 ]; then
+    echo "Output file has been created and request status code is 200"
+    exit 0
+else
+    echo "The service did not respond as expected"
+    exit 1
+fi
